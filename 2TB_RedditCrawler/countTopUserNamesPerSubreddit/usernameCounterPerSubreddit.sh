1e0c97bd91acc8344ef57849e9d58a8bf1f6bc45
Roy Rinberg <royrinberg@gmail.com>
2018-04-07 18:11:20 -0400

    changed bash

--- 2TB_RedditCrawler/countTopUserNamesPerSubreddit/usernameCounterPerSubreddit.sh
+++ 2TB_RedditCrawler/countTopUserNamesPerSubreddit/usernameCounterPerSubreddit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=G09Test
+#SBATCH --job-name=username_Counter
 #SBATCH —nodes=2
-#SBATCH --cpus-per-task=25
+#SBATCH --cpus-per-task= 180
 #SBATCH --mem=50GB
-#SBATCH --time=72:00:00
+#SBATCH --time=10:00:00
 
 
 for filename in /beegfs/avt237/data/RC* ; do
-	srun --ntasks=1 --cpus-per-task=4 --exclusive --mem=11Gb python usernameCounterPerSubreddit.py "$filename" 
+	srun --ntasks=1 --cpus-per-task=1 --exclusive --mem=11Gb python usernameCounterPerSubreddit.py "$filename" 
 done
 
 wait

eadc12defd4de035e88207239e00bf27577dca42
Roy Rinberg <royrinberg@gmail.com>
2018-04-25 19:39:22 -0400

    made a mistake writing the filename - skipped over the date

--- 2TB_RedditCrawler/statisticalLearners/statisticalLearner_subreddit.sh
+++ 2TB_RedditCrawler/statisticalLearners/statisticalLearner_subreddit.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8GB
-#SBATCH --time=48:00:00
+#SBATCH --mem=16GB
+#SBATCH --time=167:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rr2635@nyu.edu
 #SBATCH --array=0-45
